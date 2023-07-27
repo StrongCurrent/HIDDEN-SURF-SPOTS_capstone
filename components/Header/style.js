4e7dd7451f0a5a4cdef1567d6b183c76b14855eb
Martin Schilz <m.schilz@gmx.de>
2023-07-27 21:10:40 +0200

    header reorganise

---
+++ components/Header/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.h1`
+  font-weight: 500;
+  letter-spacing: 0.05rem;
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 1.4rem;
+  margin: 50px 0 10px;
+`;
+
