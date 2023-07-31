6b40775cf3bf17f3dfb4f313aab9a8eb40705f8c
Martin Schilz <m.schilz@gmx.de>
2023-07-31 13:04:16 +0200

    Add PUT method to [SpotId]

--- pages/api/spots/[spotId].js
+++ pages/api/spots/[spotId].js
@@ -22,5 +22,28 @@ export default async function handler(request, response) {
     return response.status(200).json({ message: "Spot deleted successfully" });
   }
 
-  return response.status(404).json({ message: "404 Page not found" });
+  if (request.method === "PUT") {
+    const { info } = request.body;
+
+    try {
+      const updatedSpot = await Spot.findByIdAndUpdate(
+        spotId,
+        { $push: { informations: { info } } }, 
+        { new: true }
+      );
+      
+      if (!updatedSpot) {
+        response.status(404).json({ message: "No spot found to update" });
+      } else {
+        response.status(200).json(updatedSpot);
+      }
+
+    } catch (error) {
+      response.status(500).json({ message: "Internal server error", error: error.message });
+    }
+  }
+
+  if (!["GET", "DELETE", "PUT"].includes(request.method)) {
+    return response.status(404).json({ message: "404 Page not found" });
+  }
 }
