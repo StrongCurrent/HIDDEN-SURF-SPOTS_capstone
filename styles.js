a21e8ee0517e2517f89169abb1196e084fdb8cfe
Martin Schilz <m.schilz@gmx.de>
2023-08-12 10:19:56 +0200

    font organisation

--- styles.js
+++ styles.js
@@ -1,21 +1,8 @@
 import { createGlobalStyle } from "styled-components";
-import { Roboto_Flex, Poppins } from "next/font/google";
+import { Roboto_Flex } from "next/font/google";
 
 const roboto = Roboto_Flex({
-  subsets: [
-    "cyrillic",
-    "cyrillic-ext",
-    "greek",
-    "latin",
-    "latin-ext",
-    "vietnamese",
-  ],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-});
-
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin", "latin-ext"],
 });
 
 export default createGlobalStyle`
@@ -27,7 +14,7 @@ export default createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
-    font-family: ${roboto.style.fontFamily}, ${poppins.style.fontFamily}, Arial, sans-serif;
+    font-family: ${roboto.style.fontFamily}, Arial, sans-serif;
   }
 
   h2 {
@@ -36,4 +23,8 @@ export default createGlobalStyle`
     text-transform: uppercase;
     margin: 20px 0px 20px 0px;
   }
+
+  button {
+    font-family: var(--font-roboto), Arial, sans-serif !important;
+}
 `;
